835aa9abe32e90bd10a019ed6137f5a4ee39a3a1
= <=>
2023-11-17 15:38:48 +0530

    serving html done

---
+++ socket-io-example/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createServer } = require("http");
+const { join } = require("path");
+const app = express();
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+server.listen(3000, () => {
+  console.log(`server running at http://localhost:3000`);
+});
