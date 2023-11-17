e39173ebf05ce964dd0851a4c59daaa34fb7cc90
= <=>
2023-11-17 15:03:29 +0530

    ws-1 complete

--- ws-1/server/index.js
+++ ws-1/server/index.js
@@ -1,9 +1,20 @@
+// Import the 'ws' library to create a WebSocket server
 const ws = require("ws");
+
+// Create a WebSocket server instance listening on port 3000
 const server = new ws.Server({ port: "3000" });
 
+// Event listener for when a new WebSocket connection is established
 server.on("connection", (socket) => {
+  // Event listener for when a message is received from a client
   socket.on("message", (message) => {
-    console.log(message);
+    // Convert the received message to a Buffer
+    const buffer = Buffer.from(message);
+
+    // Log the message as a string to the console
+    console.log(buffer.toString());
+
+    // Send the received message back to the client
     socket.send(`${message}`);
   });
 });
