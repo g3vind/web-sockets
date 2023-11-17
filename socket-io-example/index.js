51388810aaba8a93752e563e6aa560bcac98e35d
= <=>
2023-11-17 16:15:50 +0530

    broadcasting

--- socket-io-example/index.js
+++ socket-io-example/index.js
@@ -18,9 +18,15 @@ app.get("/", (req, res) => {
 //   });
 // });
 
+// io.on("connection", (socket) => {
+//   socket.on("chat message", (msg) => {
+//     console.log("message: " + msg);
+//   });
+// });
+
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
 });
 
